1131de0773446f69d05301e4a090be2a2cacd686
Joshua D Schoep <joshuaschoep@gmail.com>
2023-09-09 16:17:00 -0600

    new attempt

--- .github/scripts/uploads3.sh
+++ .github/scripts/uploads3.sh
@@ -3,5 +3,5 @@ ls
 echo $STATIC_FOLDER;
 echo $INDEX_FILE;
 
-aws s3 sync $STATIC_FOLDER s3://$AWS_S3_BUCKET/
-aws s3 sync $INDEX_FILE s3://$AWS_S3_BUCKET/
+aws s3 sync $STATIC_FOLDER s3://$AWS_S3_BUCKET
+aws s3 sync $INDEX_FILE s3://$AWS_S3_BUCKET
